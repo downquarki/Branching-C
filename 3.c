c170a1d22c853b1ed16c26aceb7fdf4e6b952323
downquarki <45574918+downquarki@users.noreply.github.com>
2019-01-09 21:30:29 +0100

    Update and rename 3. to 3.c

--- 3.c
+++ 3.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-#include <math.h>	
-
 int main()
 	{
 		int a,b;
-		printf("Unesite dva broja: \n");
+		printf("Insert 2 numbers: \n");
 		scanf("%d%d",&a,&b);		
 		if(a>b) printf("%d is the maximum and %d is minimum number.\n",a,b);
 		else printf("%d is the maximum and %d is the minimum number.\n",b,a);
